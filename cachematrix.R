cf5fa7eba3b2815202aae009ebc2288b826f054a
JLeshin <jonah.leshin@gmail.com>
2014-11-22 14:47:21 -0500

    JLeshin_HW2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function creates an input that goes into the second function. By 
+##first entering a matrix into the first function and then entering the output 
+##of the first function into the second function, the second function will 
+##output the inverse of the original matrix. If this inverse has not yet been
+##computed, the second function will do the computation. If we have already
+##computed the inverse of the matrix, the second function will simply print out 
+##this inverse by using the "getinverse" function from makeCacheMatrix. 
 
-## Write a short comment describing this function
+## This function produces three functions, "get"; "setinverse"; and "getinverse"
+##that will be used in the second function. Note: I have omitted the "set" 
+##function within makeCacheMatrix because it is not necessary. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        I <- NULL
+        
+        get <- function() {x}
+        setinverse <- function(inverse) I <<- inverse
+        getinverse <- function() I
+        list( get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+
+
+## This function first checks if the inverse of x has already been computed.
+##If so, it prints out the inverse; if not, it computes the inverse and then
+##prints it. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        I <- x$getinverse()
+        if(!is.null(I)) {
+                message("getting cached data")
+                return(I)
+        }
+        data <- x$get()
+        I <- solve(data, ...)
+        x$setinverse(I)
+        I
 }
+
